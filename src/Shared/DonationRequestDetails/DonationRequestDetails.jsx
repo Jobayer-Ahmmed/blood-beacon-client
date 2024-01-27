f1d0a7ed04ddfddde9602dc9df83a50759f4af1c
%Sa2323ma <jobayer.ahmmed.jas@gmail.com>
2024-01-27 20:37:58 +0600

    update: donationRequestDetails

--- src/Shared/DonationRequestDetails/DonationRequestDetails.jsx
+++ src/Shared/DonationRequestDetails/DonationRequestDetails.jsx
@@ -12,27 +12,27 @@ const DonationRequestDetails = () => {
                 <div className="flex flex-col items-center">
                 <h3 className="text-xl text-gray-600 font-bold mb-3 underline">Reciepent Information</h3>
                 <div>
-                    <p className="mt-1 text-gray-600 font-bold">Reciepent Name : {recipient_name}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Reciepent Upzila : {upzilas}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Reciepent District : {districts}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Reciepent Address : {address}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Reciepent Hospital : {hospital_name}</p>
+                    <p className="mt-1 text-gray-600 font-bold">Reciepent Name : <span className="text-gray-500"> {recipient_name}</span></p>
+                    <p className="mt-1 text-gray-600 font-bold">Reciepent Upzila : <span className="text-gray-500"> {upzilas}</span></p>
+                    <p className="mt-1 text-gray-600 font-bold">Reciepent District : <span className="text-gray-500">{districts}</span></p>
+                    <p className="mt-1 text-gray-600 font-bold">Reciepent Address : <span className="text-gray-500">{address}</span></p>
+                    <p className="mt-1 text-gray-600 font-bold">Reciepent Hospital : <span className="text-gray-500">{hospital_name}</span></p>
                 </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <h3 className="text-xl text-gray-600 font-bold mb-3 underline">Donor Information</h3>
                     <div>
-                    <p className="mt-1 text-gray-600 font-bold">Donor Name : {name}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Donor Email : {email}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Donor Message : {request_message}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Donor Status : {donation_status}</p>
+                    <p className="mt-1 text-gray-600 font-bold">Donor Name : <span className="text-gray-500">{name}</span></p>
+                    <p className="mt-1 text-gray-600 font-bold">Donor Email : <span className="text-gray-500">{email}</span> </p>
+                    <p className="mt-1 text-gray-600 font-bold">Donor Message : <span className="text-gray-500">{request_message}</span> </p>
+                    <p className="mt-1 text-gray-600 font-bold">Donor Status : <span className="text-gray-500">{donation_status}</span></p>
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
                     <h3 className="text-xl text-gray-600 font-bold mb-3 underline">Time Information</h3>
                     <div>
-                    <p className="mt-1 text-gray-600 font-bold">Donation Date : {donation_date}</p>
-                    <p className="mt-1 text-gray-600 font-bold">Donation Time : {donation_time}</p>
+                    <p className="mt-1 text-gray-600 font-bold">Donation Date : <span className="text-gray-500">{donation_date}</span> </p>
+                    <p className="mt-1 text-gray-600 font-bold">Donation Time : <span className="text-gray-500">{donation_time}</span></p>
                     </div>
                 </div>
             </div>
