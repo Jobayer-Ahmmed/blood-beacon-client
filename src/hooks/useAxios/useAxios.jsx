24f28e6da5660c204b896992769d55bc88cb242a
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-04-16 17:32:06 +0600

    update: all

--- src/hooks/useAxios/useAxios.jsx
+++ src/hooks/useAxios/useAxios.jsx
@@ -5,8 +5,8 @@ import axios from "axios";
 const useAxios=()=>{
 
     const createAxios = axios.create({
-        baseURL:"http://localhost:5000"
-        // baseURL:"https://ass-12-server-zeta.vercel.app"
+        // baseURL:"http://localhost:5000"
+        baseURL:"https://ass-12-server-zeta.vercel.app"
 
     })
     return createAxios
